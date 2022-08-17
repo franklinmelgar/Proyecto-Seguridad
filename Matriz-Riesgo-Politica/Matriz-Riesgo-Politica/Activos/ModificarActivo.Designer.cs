fef0e5933ee5acf76968fe6e4c014513eb427665
Jisslicona99 <53011909+Jisslicona99@users.noreply.github.com>
2022-08-16 21:09:19 -0600

    0909
    0909

---
+++ Matriz-Riesgo-Politica/Matriz-Riesgo-Politica/Activos/ModificarActivo.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Matriz_Riesgo_Politica.Activos
+{
+    partial class ModificarActivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCrear = new System.Windows.Forms.Button();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreActivo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btCrear
+            // 
+            this.btCrear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btCrear.Location = new System.Drawing.Point(304, 155);
+            this.btCrear.Name = "btCrear";
+            this.btCrear.Size = new System.Drawing.Size(169, 42);
+            this.btCrear.TabIndex = 9;
+            this.btCrear.Text = "Actualizar Activo";
+            this.btCrear.UseVisualStyleBackColor = true;
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(38, 111);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(435, 22);
+            this.txtCosto.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Costo";
+            // 
+            // txtNombreActivo
+            // 
+            this.txtNombreActivo.Location = new System.Drawing.Point(38, 45);
+            this.txtNombreActivo.Name = "txtNombreActivo";
+            this.txtNombreActivo.Size = new System.Drawing.Size(435, 22);
+            this.txtNombreActivo.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre Activo";
+            // 
+            // ModificarActivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 217);
+            this.Controls.Add(this.btCrear);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombreActivo);
+            this.Controls.Add(this.label1);
+            this.Name = "ModificarActivo";
+            this.Text = "ModificarActivo";
+            this.Load += new System.EventHandler(this.ModificarActivo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCrear;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreActivo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Matriz-Riesgo-Politica/Matriz-Riesgo-Politica/Activos/ModificarActivo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matriz_Riesgo_Politica.Activos
+{
+    public partial class ModificarActivo : Form
+    {
+        public int CodigoActivo;
+        public ModificarActivo()
+        {
+            InitializeComponent();
+        }
+
+        private void ModificarActivo_Load(object sender, EventArgs e)
+        {
+            Gestion_RiesgosEntities BD = new Gestion_RiesgosEntities();
+
+            if (CodigoActivo == null)
+            {
+                this.Close();
+            }
+            else
+            {
+                var activoEncontrado = BD.Activos.Where(a => a.codigoActivo.Equals(CodigoActivo)).FirstOrDefault();
+
+                txtNombreActivo.Text = activoEncontrado.nombreActivo;
+                txtCosto.Text = activoEncontrado.costoActivo.ToString();
+                txtNombreActivo.Focus();
+
+            }
+
+        }
+    }
+}
